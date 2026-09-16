39f511d019b760a53c2c4f4e41d66fa08cee7270
Enel Almonte <enelramon@hotmail.com>
2026-09-15 21:22:25 -0400

    Update Result class with additional methods for handling success and failure, and add ResultExtensions for streamlined result conversion

--- Aplicada1.Core/Result.cs
+++ Aplicada1.Core/Result.cs
@@ -20,6 +20,101 @@ public class Result
 
     public Error Error { get; }
 
+    public Result OnSuccess(Action action)
+    {
+        if (IsSuccess)
+        {
+            action();
+        }
+
+        return this;
+    }
+
+    public Result OnFailure(Action action)
+    {
+        if (IsFailure)
+        {
+            action();
+        }
+
+        return this;
+    }
+
+    public async Task<Result> OnSuccessAsync(Func<Task> action)
+    {
+        if (IsSuccess)
+        {
+            await action();
+        }
+
+        return this;
+    }
+
+    public async Task<Result> OnFailureAsync(Func<Task> action)
+    {
+        if (IsFailure)
+        {
+            await action();
+        }
+
+        return this;
+    }
+
+    public Result Bind(Func<Result> next)
+    {
+        return IsSuccess ? next() : this;
+    }
+
+    public Result<T> Bind<T>(Func<Result<T>> next)
+    {
+        return IsSuccess ? next() : Result.Failure<T>(Error);
+    }
+
+    public Result<T> Bind<T>(Func<T, Result<T>> next)
+    {
+        return IsSuccess ? next(default!) : Result.Failure<T>(Error);
+    }
+
+    public TOut Match<TOut>(Func<TOut> onSuccess, Func<Error, TOut> onFailure)
+    {
+        return IsSuccess ? onSuccess() : onFailure(Error);
+    }
+
+    public Result Finally(Action action)
+    {
+        action();
+        return this;
+    }
+
+    public Result<T> Then<T>(Func<T> onSuccess)
+    {
+        return IsSuccess ? Result.Success(onSuccess()) : Result.Failure<T>(Error);
+    }
+
+    public static Result Try(Func<Result> action)
+    {
+        try
+        {
+            return action();
+        }
+        catch (Exception ex)
+        {
+            return Failure(new Error("EXCEPTION", ex.Message));
+        }
+    }
+
+    public static Result<T> Try<T>(Func<T> action)
+    {
+        try
+        {
+            return Result.Success(action());
+        }
+        catch (Exception ex)
+        {
+            return Result.Failure<T>(new Error("EXCEPTION", ex.Message));
+        }
+    }
+
     public static Result Success() => new(true, Error.None);
 
     public static Result Failure(Error error) => new(false, error);
@@ -46,6 +141,92 @@ public class Result<T> : Result
         : throw new InvalidOperationException("Cannot access value of failed result");
 
     public T? ValueOrDefault => _value;
+
+    public Result<T> OnSuccess(Action<T> action)
+    {
+        if (IsSuccess)
+        {
+            action(Value);
+        }
+
+        return this;
+    }
+
+    public Result<T> OnFailure(Action<Error> action)
+    {
+        if (IsFailure)
+        {
+            action(Error);
+        }
+
+        return this;
+    }
+
+    public async Task<Result<T>> OnSuccessAsync(Func<T, Task> action)
+    {
+        if (IsSuccess)
+        {
+            await action(Value);
+        }
+
+        return this;
+    }
+
+    public async Task<Result<T>> OnFailureAsync(Func<Error, Task> action)
+    {
+        if (IsFailure)
+        {
+            await action(Error);
+        }
+
+        return this;
+    }
+
+    public Result<U> Map<U>(Func<T, U> map)
+    {
+        return IsSuccess ? Result.Success(map(Value)) : Result.Failure<U>(Error);
+    }
+
+    public Result<U> Bind<U>(Func<T, Result<U>> next)
+    {
+        return IsSuccess ? next(Value) : Result.Failure<U>(Error);
+    }
+
+    public Result<T> Tap(Action<T> action)
+    {
+        if (IsSuccess)
+        {
+            action(Value);
+        }
+
+        return this;
+    }
+
+    public Result<T> TapError(Action<Error> action)
+    {
+        if (IsFailure)
+        {
+            action(Error);
+        }
+
+        return this;
+    }
+
+    public TOut Match<TOut>(Func<T, TOut> onSuccess, Func<Error, TOut> onFailure)
+    {
+        return IsSuccess ? onSuccess(Value) : onFailure(Error);
+    }
+
+    public Result<U> Then<U>(Func<T, U> onSuccess)
+    {
+        return IsSuccess ? Result.Success(onSuccess(Value)) : Result.Failure<U>(Error);
+    }
+
+    public new Result<T> Finally(Action action)
+    {
+        action();
+        return this;
+    }
 
     public static implicit operator Result<T>(T value) => Success(value);
 
